2664f44d8ec16914064a74047c57ef8032e05ccf
norels <aelizzah@gmail.com>
2020-10-07 08:35:56 +0700

    pertemuan pertama

---
+++ variable.py
@@ -0,0 +1,20 @@
+#x = 5
+#y = "Hello, Elsss"
+
+#print(x)
+#print(y)
+
+#print(y)
+#print(y)
+#print(y)
+
+a="aku"
+b="pusing"
+c="belajar"
+d="phyton"
+
+x="tapi"
+y="seneng"
+
+print(a,b,c,d)
+print(x,y)
