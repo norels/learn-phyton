0256eb749a44262bbf1d151f326744521c82ae45
norels <aelizzah@gmail.com>
2020-10-09 23:05:47 +0700

    Revisi Tugas No 3

--- Tugas 1/Soal3.py
+++ Tugas 1/Soal3.py
@@ -2,15 +2,13 @@ teori = int(input("Berapa nilai teori: "))
 praktik = int(input("Berapa nilai praktik:"))
 
 
-if teori > 70 and praktik > 70:
+if teori >= 70 and praktik >= 70:
     print("Selamat, anda lulus!")
 elif teori < 70 and praktik >= 70:
     print("Anda harus mengulang ujian teori")
 elif teori >= 70 and praktik < 70:
     print("Anda harus mengulang ujian praktik")
-elif teori < 70 and praktik < 70:
+else :
     print("Anda harus mengulang ujian teori dan ujian praktik")
 
-
-
-    
+#udah bisa kayaknya hehehe
