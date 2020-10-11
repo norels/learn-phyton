11537c601dd22f04bf8177cc9e885176f91deabf
norels <aelizzah@gmail.com>
2020-10-11 13:12:29 +0700

    Pertemuan ketiga

---
+++ for_loop.py
@@ -0,0 +1,15 @@
+nama_hewan = ["kucing", "kelinci", "kambing", "sapi", "kuda", "jerapah"]
+
+print(nama_hewan[0])
+print(nama_hewan[1])
+print(nama_hewan[2])
+print(nama_hewan[3])
+print(nama_hewan[4])
+print(nama_hewan[5])
+print(len(nama_hewan))
+print(nama_hewan)
+
+#for hewan in nama_hewan:
+#    print(hewan)
+
+#print("done")
---
+++ range.py
@@ -0,0 +1,13 @@
+#for i in range(9):
+#     print(i)
+
+print("----")
+
+#for i in range(98,100):
+#     print(i)
+
+#for i in range(1, 20, 3):
+#     print(i)
+
+for i in range(70,77):
+    print(i)
